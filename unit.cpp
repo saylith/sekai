f9647fc300e084ed3571eff2d55e5ea514c94680
saylith <saylith@gmail.com>
2014-06-26 18:04:24 -0700

    added unit.cpp

---
+++ unit.cpp
@@ -0,0 +1,12 @@
+class Unit
+{
+    int movement;
+    int attack;
+    int quantity;
+
+    WeaponType weaponType;
+    ArmorType armorType;
+    TerrainBonuses terrainBonuses;
+
+    char representation;
+}
