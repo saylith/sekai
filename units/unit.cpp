8cff0a69f512923365151cfdfc78b641a6395222
saylith <saylith@gmail.com>
2014-06-26 18:22:33 -0700

    more changes

--- units/unit.cpp
+++ units/unit.cpp
@@ -4,6 +4,7 @@ class Unit
     int attack;
     int hp, maxHp;
     int mp, maxMp;
+    int actions;
 
     WeaponType weaponType;
     ArmorType armorType;
@@ -14,11 +15,29 @@ class Unit
 
     public:
         int getAttackDamage(Unit enemy, Terrain terrain);
+        int calculateDamageTaken(Unit enemy, Terrain terrain);
+
 
         int getMovement();
-        int changeMovement(change);
-        
-        int changeHP(int change);
-        int changeMP(int change);
+        int getAttack();
+        int getHp();
+        int getMaxHp();
+        int getMp();
+        int getMaxMP();
+        int getActions();
 
+        WeaponType getWeaponType();
+        ArmorType getArmorType();
+        TerrainBonuses getTerrainBonuses();
+        Status getStatus();
+
+        int setMovement();
+        int setAttack();
+        int setHp();
+        int setMaxHp();
+        int setMp();
+        int setMaxMP();
+        int setActions();
+
+        Status setStatus();
 }
